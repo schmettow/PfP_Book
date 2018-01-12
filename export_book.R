66f0a433941131ef889614d5f0deb2fe54bb10f5
schmettow <schmettow@web.de>
2018-01-12 10:32:21 +0100

    + Chapters 01 - 06, 09 workably complete (Lena) + Ch 07 Working with data started
    - complete Ch 07 by introducing lists of lists and csv export
    - create GIT book hosting

---
+++ export_book.R
@@ -0,0 +1,25 @@
+file.remove("_main.Rmd")
+
+### HTML
+
+bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = TRUE,
+            output_dir = "../Public/PfP_book/HTML/", 
+            encoding = "UTF-8")
+
+### Epub
+
+bookdown::render_book("index.Rmd", output_format = "bookdown::epub_book", clean = TRUE,
+                      output_dir = "../Public/PfP_book/Epub/", 
+                      encoding = "UTF-8")
+
+### Kindle
+
+bookdown::kindlegen(epub = "../Public/PfP_book/Epub/_main.epub")
+
+### PDF
+### as LaTeX is not working, we do a workaround via Word, which needs manual conversion to PDF
+
+bookdown::render_book("index.Rmd", output_format = "bookdown::word_document2", clean = T,
+                     output_dir = "../Public/PfP_book/PDF",
+                    encoding = "UTF-8")
+
